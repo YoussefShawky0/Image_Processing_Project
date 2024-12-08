a5b085586d6a855cd86baac173d4ce0b2d2581b7
ZiadShawkii23 <ziadshawky49@gmail.com>
2024-12-08 05:31:04 -0800

    adding Account Zizou

--- app/src/classes/Account.py
+++ app/src/classes/Account.py
@@ -1,34 +1,32 @@
 class Account:
-    def __init__(self, account_id, username, password, card_number, pin, balance):
-        self.account_id = account_id
-        self.username = username
-        self.password = password
-        self.card_number = card_number
-        self.pin = pin
-        self.balance = balance
+    def __init__(self, account_id: int, username: str, password: str, card_number: int, pin: int, balance: int):
+        self.__account_id__ = account_id
+        self.__username__ = username
+        self.__password__ = password
+        self.__card_number__ = card_number
+        self.__pin__ = pin
+        self.__balance__ = balance
 
-    def get_account_id(self):
-        return self.account_id
+    def get_account_id(self) -> int:
+        return self.__account_id__
 
-    def get_username(self):
-        return self.username
+    def get_username(self) -> str:
+        return self.__username__
 
-    def get_password(self):
-        return self.password
+    def get_password(self) -> str:
+        return self.__password__
 
-    def get_card_number(self):
-        return self.card_number
+    def get_card_number(self) -> int:
+        return self.__card_number__
 
-    def get_pin(self):
-        return self.pin
+    def get_pin(self) -> int:
+        return self.__pin__
 
-    def get_balance(self):
-        return self.balance
+    def get_balance(self) -> int:
+        return self.__balance__
 
-    def deposit(self, amount):
-        # Implement logic to deposit money
-        pass
+    def deposit(self, amount: int) -> None:
+        self.balance += amount
 
-    def withdraw(self, amount):
-        # Implement logic to withdraw money
-        pass
+    def withdraw(self, amount: int) -> None:
+        self.balance -= amount
